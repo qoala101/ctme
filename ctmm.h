eab4a8745f0e746096bd254bf02aeeec40871fb3
Volodymyr Hromakov <4y5t6r@gmail.com>
2022-08-16 12:17:01 +0300

    Changed expression to contain mats or other expressions. Changed API to work only with vectors of ints. Value containers are passed as constructor arguments.

--- ctmm.h
+++ ctmm.h
@@ -2,46 +2,126 @@
 #define CTMM_H_
 
 #include <array>
+#include <utility>
+#include <vector>
 
 namespace ctmm {
-template <typename T, int Rows, int Cols>
-struct Mat {};
+using ClientVal = int;
+using ClientVec = std::vector<ClientVal>;
+using ClientMat = std::vector<ClientVec>;
 
-template <typename T, int RowsLeft, int Common, int ColsRight, int RowIndex,
-          int ColIndex>
+template <int Rows, int Cols, int RowIndex, int ColIndex>
+struct Col {
+  explicit Col(const ClientMat& client_mat) : client_mat_{client_mat} {}
+
+  auto GetVal() const { return client_mat_[RowIndex][ColIndex]; }
+
+  const ClientMat& client_mat_;
+};
+
+template <int Rows, int Cols, int RowIndex>
+struct Row {
+  explicit Row(const ClientMat& client_mat) : client_mat_{client_mat} {}
+
+  template <int ColIndex>
+  auto GetCol() const {
+    return Col<Rows, Cols, RowIndex, ColIndex>{client_mat_};
+  }
+
+  const ClientMat& client_mat_;
+};
+
+template <int Rows, int Cols>
+struct Mat {
+  static const constinit auto kRows = Rows;
+  static const constinit auto kCols = Cols;
+
+  template <int RowIndex>
+  using RowType = Row<Rows, Cols, RowIndex>;
+
+  explicit Mat(const ClientMat& client_mat) : client_mat_{client_mat} {}
+
+  template <int RowIndex>
+  auto GetRow() const {
+    return Row<Rows, Cols, RowIndex>{client_mat_};
+  }
+
+  const ClientMat& client_mat_;
+};
+
+// template <typename T, int RowsLeft, int Common, int ColsRight, int RowIndex,
+//           int ColIndex>
+// struct ColExpression {
+//    auto operator()(const auto& left, const auto& right) const {
+//     auto sum = T{};
+
+//     for (auto i = 0; i < Common; ++i) {
+//       sum += left[RowIndex][i] * right[i][ColIndex];
+//     }
+
+//     return sum;
+//   }
+// };
+
+template <typename LeftMat, typename RightMat, int RowIndex, int ColIndex>
 struct ColExpression {
-  constexpr auto operator()(const auto& left, const auto& right) const {
-    auto sum = T{};
+  explicit ColExpression(LeftMat left_mat, RightMat right_mat)
+      : left_mat_{std::move(left_mat)}, right_mat_{std::move(right_mat)} {}
 
-    for (auto i = 0; i < Common; ++i) {
-      sum += left[RowIndex][i] * right[i][ColIndex];
+  auto GetVal() const {
+    auto sum = 0;
+
+    for (auto i = 0; i < LeftMat::kCols; ++i) {
+      sum +=
+          left_mat_.template GetRow<RowIndex>().template GetCol</*i*/0>().GetVal() *
+          right_mat_.template GetRow</*i*/0>().template GetCol<ColIndex>().GetVal();
     }
 
     return sum;
   }
+
+  LeftMat left_mat_{};
+  RightMat right_mat_{};
 };
 
-template <typename T, int RowsLeft, int Common, int ColsRight, int RowIndex>
+template <typename LeftMat, typename RightMat, int RowIndex>
 struct RowExpression {
+  explicit RowExpression(LeftMat left_mat, RightMat right_mat)
+      : left_mat_{std::move(left_mat)}, right_mat_{std::move(right_mat)} {}
+
   template <int ColIndex>
-  constexpr auto Col() const {
-    return ColExpression<T, RowsLeft, Common, ColsRight, RowIndex, ColIndex>{};
+  auto GetCol() const {
+    return ColExpression<LeftMat, RightMat, RowIndex, ColIndex>{left_mat_,
+                                                                right_mat_};
   }
+
+  LeftMat left_mat_{};
+  RightMat right_mat_{};
 };
 
-template <typename T, int RowsLeft, int Common, int ColsRight>
+template <typename LeftMat, typename RightMat>
 struct MatExpression {
+  static const constinit auto kRows = LeftMat::kRows;
+  static const constinit auto kCols = RightMat::kCols;
+  static_assert(LeftMat::kCols == RightMat::kRows);
+
   template <int RowIndex>
-  constexpr auto Row() const {
-    return RowExpression<T, RowsLeft, Common, ColsRight, RowIndex>{};
+  using RowType = RowExpression<LeftMat, RightMat, RowIndex>;
+
+  explicit MatExpression(LeftMat left_mat, RightMat right_mat)
+      : left_mat_{std::move(left_mat)}, right_mat_{std::move(right_mat)} {}
+
+  template <int RowIndex>
+  auto GetRow() const {
+    return RowExpression<LeftMat, RightMat, RowIndex>{left_mat_, right_mat_};
   }
+
+  LeftMat left_mat_{};
+  RightMat right_mat_{};
 };
 
-template <typename T, int RowsLeft, int Common, int ColsRight>
-[[nodiscard]] constexpr auto operator*(
-    const Mat<T, RowsLeft, Common>& /*unused*/,
-    const Mat<T, Common, ColsRight>& /*unused*/) {
-  return MatExpression<T, RowsLeft, Common, ColsRight>{};
+[[nodiscard]] auto operator*(const auto& left_mat, const auto& right_mat) {
+  return MatExpression{left_mat, right_mat};
 }
 }  // namespace ctmm
 
--- tests.cc
+++ tests.cc
@@ -1,33 +1,85 @@
 #ifndef CTMM_TESTS_H_
 #define CTMM_TESTS_H_
 
+#include <cassert>
 #include <iostream>
 #include <vector>
 
 #include "ctmm.h"
 
 namespace ctmm::tests {
-void Test1() {
-  constexpr auto a = Mat<int, 2, 3>{};
-  constexpr auto b = Mat<int, 3, 2>{};
-  constexpr auto expression = a * b;
+// void Test1() {
+//   constexpr auto a = Mat<int, 2, 3>{};
+//   constexpr auto b = Mat<int, 3, 2>{};
+//   constexpr auto expression = a * b;
 
-  constexpr auto a_values = std::array<std::array<int, 3>, 2>{
-      std::array<int, 3>{1, 2, 3}, std::array<int, 3>{4, 5, 6}};
-  constexpr auto b_values = std::array<std::array<int, 2>, 3>{
-      std::array<int, 2>{1, 2}, std::array<int, 2>{3, 4},
-      std::array<int, 2>{5, 6}};
+//   constexpr auto a_values = std::array<std::array<int, 3>, 2>{
+//       std::array<int, 3>{1, 2, 3}, std::array<int, 3>{4, 5, 6}};
+//   constexpr auto b_values = std::array<std::array<int, 2>, 3>{
+//       std::array<int, 2>{1, 2}, std::array<int, 2>{3, 4},
+//       std::array<int, 2>{5, 6}};
 
-  static_assert(expression.Row<0>().Col<0>()(a_values, b_values) == 22);
-  static_assert(expression.Row<0>().Col<1>()(a_values, b_values) == 28);
-  static_assert(expression.Row<1>().Col<0>()(a_values, b_values) == 49);
-  static_assert(expression.Row<1>().Col<1>()(a_values, b_values) == 64);
+//   static_assert(expression.Row<0>().Col<0>()(a_values, b_values) == 22);
+//   static_assert(expression.Row<0>().Col<1>()(a_values, b_values) == 28);
+//   static_assert(expression.Row<1>().Col<0>()(a_values, b_values) == 49);
+//   static_assert(expression.Row<1>().Col<1>()(a_values, b_values) == 64);
+// }
+
+// void Test2() {
+//   const auto a_values = std::vector<std::vector<int>>{
+//       std::vector<int>{1, 2, 3}, std::vector<int>{4, 5, 6}};
+//   const auto b_values = std::vector<std::vector<int>>{
+//       std::vector<int>{1, 2}, std::vector<int>{3, 4}, std::vector<int>{5,
+//       6}};
+
+//   const auto a = Mat<2, 3>{a_values};
+//   const auto b = Mat<3, 2>{b_values};
+//   const auto expression = a * b;
+
+//   assert(expression.Row<0>().Col<0>()(a_values, b_values) == 22);
+//   assert(expression.Row<0>().Col<1>()(a_values, b_values) == 28);
+//   assert(expression.Row<1>().Col<0>()(a_values, b_values) == 49);
+//   assert(expression.Row<1>().Col<1>()(a_values, b_values) == 64);
+// }
+
+void Test3() {
+  const auto a_values = std::vector<std::vector<int>>{
+      std::vector<int>{1, 2, 3}, std::vector<int>{4, 5, 6}};
+  const auto b_values = std::vector<std::vector<int>>{
+      std::vector<int>{1, 2}, std::vector<int>{3, 4}, std::vector<int>{5, 6}};
+  const auto c_values = std::vector<std::vector<int>>{
+      std::vector<int>{1, 2, 3, 4}, std::vector<int>{5, 6, 7, 8}};
+
+  const auto a = Mat<2, 3>{a_values};
+  const auto b = Mat<3, 2>{b_values};
+  const auto c = Mat<2, 4>{c_values};
+  const auto expression = a * b * c;
+
+  std::cout << expression.GetRow<0>().GetCol<0>().GetVal() << "\n";
+  std::cout << expression.GetRow<0>().GetCol<1>().GetVal() << "\n";
+  std::cout << expression.GetRow<0>().GetCol<2>().GetVal() << "\n";
+  std::cout << expression.GetRow<0>().GetCol<3>().GetVal() << "\n";
+  std::cout << expression.GetRow<1>().GetCol<0>().GetVal() << "\n";
+  std::cout << expression.GetRow<1>().GetCol<1>().GetVal() << "\n";
+  std::cout << expression.GetRow<1>().GetCol<2>().GetVal() << "\n";
+  std::cout << expression.GetRow<1>().GetCol<3>().GetVal() << "\n";
+
+  assert(expression.GetRow<0>().GetCol<0>().GetVal() == 162);
+  assert(expression.GetRow<0>().GetCol<1>().GetVal() == 212);
+  assert(expression.GetRow<0>().GetCol<2>().GetVal() == 262);
+  assert(expression.GetRow<0>().GetCol<3>().GetVal() == 312);
+  assert(expression.GetRow<1>().GetCol<0>().GetVal() == 369);
+  assert(expression.GetRow<1>().GetCol<1>().GetVal() == 482);
+  assert(expression.GetRow<1>().GetCol<2>().GetVal() == 595);
+  assert(expression.GetRow<1>().GetCol<3>().GetVal() == 708);
 }
 }  // namespace ctmm::tests
 
 auto main() -> int {
   std::cout << "TEST START\n";
-  ctmm::tests::Test1();
+  // ctmm::tests::Test1();
+  // ctmm::tests::Test2();
+  ctmm::tests::Test3();
   std::cout << "TEST END\n";
   return 0;
 }
