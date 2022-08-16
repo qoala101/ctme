930a37cc2baf2a7b2dc99c5ff772df9cbb8aa72e
Volodymyr Hromakov <4y5t6r@gmail.com>
2022-08-16 17:22:14 +0300

    Added facades for simpler evaluation.

--- ctmm.h
+++ ctmm.h
@@ -11,7 +11,7 @@ namespace ctmm {
 template <int Rows, int Cols, int RowIndex, int ColIndex>
 struct Col {
   template <int ContainerIndex, typename... Args>
-  constexpr auto GetVal(const Args&... args) const {
+  constexpr auto GetVal(const Args &...args) const {
     return std::get<ContainerIndex>(std::tie(args...))[RowIndex][ColIndex];
   }
 };
@@ -44,13 +44,14 @@ template <typename LeftMat, typename RightMat, int RowIndex, int ColIndex,
 struct Iterator {
   template <typename... Args>
   constexpr explicit Iterator(LeftMat left_mat, RightMat right_mat,
-                              const Args&... args)
+                              const Args &...args)
       : left_mat_{std::move(left_mat)},
         right_mat_{std::move(right_mat)},
 
         sum_{left_mat_.template GetRow<RowIndex>()
                      .template GetCol<Index>()
-                     .template GetVal<ContainerIndex - RightMat::kContainers>(args...) *
+                     .template GetVal<ContainerIndex - RightMat::kContainers>(
+                         args...) *
                  right_mat_.template GetRow<Index>()
                      .template GetCol<ColIndex>()
                      .template GetVal<ContainerIndex>(args...) +
@@ -69,7 +70,7 @@ template <typename LeftMat, typename RightMat, int RowIndex, int ColIndex,
 struct Iterator<LeftMat, RightMat, RowIndex, ColIndex, ContainerIndex, -1> {
   template <typename... Args>
   constexpr explicit Iterator(LeftMat left_mat, RightMat right_mat,
-                              const Args&... args) {}
+                              const Args &...args) {}
 
   int sum_{};
 };
@@ -80,14 +81,14 @@ struct ColExpression {
       : left_mat_{std::move(left_mat)}, right_mat_{std::move(right_mat)} {}
 
   template <int ContainerIndex, typename... Args>
-  constexpr auto GetVal(const Args&... args) const {
+  constexpr auto GetVal(const Args &...args) const {
     return Iterator<LeftMat, RightMat, RowIndex, ColIndex, ContainerIndex,
                     LeftMat::kCols - 1>{left_mat_, right_mat_, args...}
         .sum_;
   }
 
   template <typename... Args>
-  constexpr auto GetVal(const Args&... args) const {
+  constexpr auto GetVal(const Args &...args) const {
     return GetVal<sizeof...(args) - 1>(args...);
   }
 
@@ -116,7 +117,8 @@ struct MatExpression {
 
   static const constinit auto kRows = LeftMat::kRows;
   static const constinit auto kCols = RightMat::kCols;
-  static const constinit auto kContainers = LeftMat::kContainers + RightMat::kContainers;
+  static const constinit auto kContainers =
+      LeftMat::kContainers + RightMat::kContainers;
 
   template <int RowIndex>
   using RowType = RowExpression<LeftMat, RightMat, RowIndex>;
@@ -133,9 +135,66 @@ struct MatExpression {
   RightMat right_mat_{};
 };
 
-[[nodiscard]] constexpr auto operator*(const auto& left_mat,
-                                       const auto& right_mat) {
+[[nodiscard]] constexpr auto operator*(const auto &left_mat,
+                                       const auto &right_mat) {
   return MatExpression{left_mat, right_mat};
+}
+
+template <int RowIndex, int ColIndex, typename... Args>
+[[nodiscard]] constexpr auto EvaluateCell(const auto &expression,
+                                          const Args &...args) {
+  return expression.template GetRow<RowIndex>()
+      .template GetCol<ColIndex>()
+      .template GetVal(args...);
+}
+
+template <typename Expression, int RowIndex, int ColIndex>
+struct Evaluator {
+  template <typename... Args>
+  constexpr explicit Evaluator(
+      const Expression &expression,
+      std::array<std::array<int, Expression::kCols>, Expression::kRows> &result,
+      const Args &...args) {
+    result[RowIndex][ColIndex] =
+        EvaluateCell<RowIndex, ColIndex>(expression, args...);
+    Evaluator<Expression, RowIndex, ColIndex - 1>{expression, result, args...};
+  }
+};
+
+template <typename Expression, int RowIndex>
+struct Evaluator<Expression, RowIndex, 0> {
+  template <typename... Args>
+  constexpr explicit Evaluator(
+      const Expression &expression,
+      std::array<std::array<int, Expression::kCols>, Expression::kRows> &result,
+      const Args &...args) {
+    result[RowIndex][0] = EvaluateCell<RowIndex, 0>(expression, args...);
+    Evaluator<Expression, RowIndex - 1, Expression::kCols - 1>{expression,
+                                                               result, args...};
+  }
+};
+
+template <typename Expression>
+struct Evaluator<Expression, 0, 0> {
+  template <typename... Args>
+  constexpr explicit Evaluator(
+      const Expression &expression,
+      std::array<std::array<int, Expression::kCols>, Expression::kRows> &result,
+      const Args &...args) {
+    result[0][0] = EvaluateCell<0, 0>(expression, args...);
+  }
+};
+
+template <typename Expression, typename... Args>
+[[nodiscard]] constexpr auto EvaluateMatrix(const Expression &expression,
+                                            const Args &...args) {
+  auto result =
+      std::array<std::array<int, Expression::kCols>, Expression::kRows>{};
+
+  Evaluator<Expression, Expression::kRows - 1, Expression::kCols - 1>{
+      expression, result, args...};
+
+  return result;
 }
 }  // namespace ctmm
 
--- tests.cc
+++ tests.cc
@@ -18,8 +18,7 @@ void Test2Vectors() {
 
   constexpr auto expression = Mat<2, 3>{} * Mat<3, 2>{};
 
-  std::cout << expression.GetRow<0>().GetCol<0>().GetVal(a_values, b_values)
-            << "\n";
+  std::cout << EvaluateCell<0, 0>(expression, a_values, b_values) << "\n";
   std::cout << expression.GetRow<0>().GetCol<1>().GetVal(a_values, b_values)
             << "\n";
   std::cout << expression.GetRow<1>().GetCol<0>().GetVal(a_values, b_values)
@@ -116,12 +115,12 @@ void Test2Arrays() {
 }
 
 void Test3Arrays() {
-  const auto a_values = std::array<std::array<int, 3>, 2>{
+  constexpr auto a_values = std::array<std::array<int, 3>, 2>{
       std::array<int, 3>{11, 12, 13}, std::array<int, 3>{14, 15, 16}};
-  const auto b_values = std::array<std::array<int, 2>, 3>{
+  constexpr auto b_values = std::array<std::array<int, 2>, 3>{
       std::array<int, 2>{21, 22}, std::array<int, 2>{23, 24},
       std::array<int, 2>{25, 26}};
-  const auto c_values = std::array<std::array<int, 4>, 2>{
+  constexpr auto c_values = std::array<std::array<int, 4>, 2>{
       std::array<int, 4>{31, 32, 33, 34}, std::array<int, 4>{35, 36, 37, 38}};
 
   constexpr auto expression = Mat<2, 3>{} * Mat<3, 2>{} * Mat<2, 4>{};
@@ -151,22 +150,22 @@ void Test3Arrays() {
                                                          c_values)
             << "\n";
 
-  assert(expression.GetRow<0>().GetCol<0>().GetVal(a_values, b_values,
-                                                   c_values) == 56172);
-  assert(expression.GetRow<0>().GetCol<1>().GetVal(a_values, b_values,
-                                                   c_values) == 57872);
-  assert(expression.GetRow<0>().GetCol<2>().GetVal(a_values, b_values,
-                                                   c_values) == 59572);
-  assert(expression.GetRow<0>().GetCol<3>().GetVal(a_values, b_values,
-                                                   c_values) == 61272);
-  assert(expression.GetRow<1>().GetCol<0>().GetVal(a_values, b_values,
-                                                   c_values) == 70149);
-  assert(expression.GetRow<1>().GetCol<1>().GetVal(a_values, b_values,
-                                                   c_values) == 72272);
-  assert(expression.GetRow<1>().GetCol<2>().GetVal(a_values, b_values,
-                                                   c_values) == 74395);
-  assert(expression.GetRow<1>().GetCol<3>().GetVal(a_values, b_values,
-                                                   c_values) == 76518);
+  static_assert(expression.GetRow<0>().GetCol<0>().GetVal(a_values, b_values,
+                                                          c_values) == 56172);
+  static_assert(expression.GetRow<0>().GetCol<1>().GetVal(a_values, b_values,
+                                                          c_values) == 57872);
+  static_assert(expression.GetRow<0>().GetCol<2>().GetVal(a_values, b_values,
+                                                          c_values) == 59572);
+  static_assert(expression.GetRow<0>().GetCol<3>().GetVal(a_values, b_values,
+                                                          c_values) == 61272);
+  static_assert(expression.GetRow<1>().GetCol<0>().GetVal(a_values, b_values,
+                                                          c_values) == 70149);
+  static_assert(expression.GetRow<1>().GetCol<1>().GetVal(a_values, b_values,
+                                                          c_values) == 72272);
+  static_assert(expression.GetRow<1>().GetCol<2>().GetVal(a_values, b_values,
+                                                          c_values) == 74395);
+  static_assert(expression.GetRow<1>().GetCol<3>().GetVal(a_values, b_values,
+                                                          c_values) == 76518);
 }
 
 void Test1Vector1Array() {
@@ -194,50 +193,85 @@ void Test1Vector1Array() {
 }
 
 void Test1Mat1Expression() {
-  const auto d_values = std::vector<std::vector<int>>{
-      std::vector<int>{41, 42}};
-  const auto a_values = std::vector<std::vector<int>>{
-      std::vector<int>{11, 12, 13}, std::vector<int>{14, 15, 16}};
-  const auto b_values = std::vector<std::vector<int>>{std::vector<int>{21, 22},
-                                                      std::vector<int>{23, 24},
-                                                      std::vector<int>{25, 26}};
+  constexpr auto d_values =
+      std::array<std::array<int, 2>, 1>{std::array<int, 2>{41, 42}};
+  constexpr auto a_values = std::array<std::array<int, 3>, 2>{
+      std::array<int, 3>{11, 12, 13}, std::array<int, 3>{14, 15, 16}};
+  constexpr auto b_values = std::array<std::array<int, 2>, 3>{
+      std::array<int, 2>{21, 22}, std::array<int, 2>{23, 24},
+      std::array<int, 2>{25, 26}};
 
   constexpr auto expression1 = Mat<2, 3>{} * Mat<3, 2>{};
   constexpr auto expression = Mat<1, 2>{} * expression1;
 
-  std::cout << expression.GetRow<0>().GetCol<0>().GetVal(d_values, a_values, b_values)
+  std::cout << expression.GetRow<0>().GetCol<0>().GetVal(d_values, a_values,
+                                                         b_values)
             << "\n";
-  std::cout << expression.GetRow<0>().GetCol<1>().GetVal(d_values, a_values, b_values)
+  std::cout << expression.GetRow<0>().GetCol<1>().GetVal(d_values, a_values,
+                                                         b_values)
             << "\n";
 
-  assert(expression.GetRow<0>().GetCol<0>().GetVal(d_values, a_values, b_values) == 77750);
-  assert(expression.GetRow<0>().GetCol<1>().GetVal(d_values, a_values, b_values) == 81116);
+  static_assert(expression.GetRow<0>().GetCol<0>().GetVal(d_values, a_values,
+                                                          b_values) == 77750);
+  static_assert(expression.GetRow<0>().GetCol<1>().GetVal(d_values, a_values,
+                                                          b_values) == 81116);
 }
 
 void Test2Expressions() {
-  const auto a_values = std::vector<std::vector<int>>{
-      std::vector<int>{11, 12, 13}, std::vector<int>{14, 15, 16}};
-  const auto b_values = std::vector<std::vector<int>>{std::vector<int>{21, 22},
-                                                      std::vector<int>{23, 24},
-                                                      std::vector<int>{25, 26}};
+  constexpr auto a_values = std::array<std::array<int, 3>, 2>{
+      std::array<int, 3>{11, 12, 13}, std::array<int, 3>{14, 15, 16}};
+  constexpr auto b_values = std::array<std::array<int, 2>, 3>{
+      std::array<int, 2>{21, 22}, std::array<int, 2>{23, 24},
+      std::array<int, 2>{25, 26}};
 
   constexpr auto expression1 = Mat<2, 3>{} * Mat<3, 2>{};
   constexpr auto expression2 = Mat<2, 3>{} * Mat<3, 2>{};
   constexpr auto expression = expression1 * expression2;
 
-  std::cout << expression.GetRow<0>().GetCol<0>().GetVal(a_values, b_values, a_values, b_values)
+  std::cout << expression.GetRow<0>().GetCol<0>().GetVal(a_values, b_values,
+                                                         a_values, b_values)
             << "\n";
-  std::cout << expression.GetRow<0>().GetCol<1>().GetVal(a_values, b_values, a_values, b_values)
+  std::cout << expression.GetRow<0>().GetCol<1>().GetVal(a_values, b_values,
+                                                         a_values, b_values)
             << "\n";
-  std::cout << expression.GetRow<1>().GetCol<0>().GetVal(a_values, b_values, a_values, b_values)
+  std::cout << expression.GetRow<1>().GetCol<0>().GetVal(a_values, b_values,
+                                                         a_values, b_values)
             << "\n";
-  std::cout << expression.GetRow<1>().GetCol<1>().GetVal(a_values, b_values, a_values, b_values)
+  std::cout << expression.GetRow<1>().GetCol<1>().GetVal(a_values, b_values,
+                                                         a_values, b_values)
             << "\n";
 
-  assert(expression.GetRow<0>().GetCol<0>().GetVal(a_values, b_values, a_values, b_values) == 1594076);
-  assert(expression.GetRow<0>().GetCol<1>().GetVal(a_values, b_values, a_values, b_values) == 1663088);
-  assert(expression.GetRow<1>().GetCol<0>().GetVal(a_values, b_values, a_values, b_values) == 1990724);
-  assert(expression.GetRow<1>().GetCol<1>().GetVal(a_values, b_values, a_values, b_values) == 2076908);
+  static_assert(expression.GetRow<0>().GetCol<0>().GetVal(
+                    a_values, b_values, a_values, b_values) == 1594076);
+  static_assert(expression.GetRow<0>().GetCol<1>().GetVal(
+                    a_values, b_values, a_values, b_values) == 1663088);
+  static_assert(expression.GetRow<1>().GetCol<0>().GetVal(
+                    a_values, b_values, a_values, b_values) == 1990724);
+  static_assert(expression.GetRow<1>().GetCol<1>().GetVal(
+                    a_values, b_values, a_values, b_values) == 2076908);
+}
+
+void TestFacade() {
+  constexpr auto a_values = std::array<std::array<int, 3>, 2>{
+      std::array<int, 3>{11, 12, 13}, std::array<int, 3>{14, 15, 16}};
+  constexpr auto b_values = std::array<std::array<int, 2>, 3>{
+      std::array<int, 2>{21, 22}, std::array<int, 2>{23, 24},
+      std::array<int, 2>{25, 26}};
+  constexpr auto c_values = std::array<std::array<int, 4>, 2>{
+      std::array<int, 4>{31, 32, 33, 34}, std::array<int, 4>{35, 36, 37, 38}};
+
+  constexpr auto expression = Mat<2, 3>{} * Mat<3, 2>{} * Mat<2, 4>{};
+
+  constexpr auto result = EvaluateMatrix(expression, a_values, b_values, c_values);
+
+  static_assert(result[0][0] == 56172);
+  static_assert(result[0][1] == 57872);
+  static_assert(result[0][2] == 59572);
+  static_assert(result[0][3] == 61272);
+  static_assert(result[1][0] == 70149);
+  static_assert(result[1][1] == 72272);
+  static_assert(result[1][2] == 74395);
+  static_assert(result[1][3] == 76518);
 }
 }  // namespace ctmm::tests
 
@@ -250,6 +284,7 @@ auto main() -> int {
   ctmm::tests::Test1Vector1Array();
   ctmm::tests::Test1Mat1Expression();
   ctmm::tests::Test2Expressions();
+  ctmm::tests::TestFacade();
   std::cout << "TEST END\n";
   return 0;
 }
